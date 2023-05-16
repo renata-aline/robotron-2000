9477a85417645236dc0f113a063c8d1adcbc67b4
renata-aline <renataaline@gmail.com>
2023-05-16 13:38:44 -0300

    Alterando item na página

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 
+// const robotron = document.querySelector("#robotron");
 
-const robotron = document.querySelector("#robotron");
+// robotron.addEventListener("click", dizOi);
 
-robotron.addEventListener("click", dizOi);
+// function dizOi() {
+//   console.log("oi");
+// }
 
-function dizOi() {
-  console.log("oi");
-}
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+somar.addEventListener('click',(evento) => {
+    braco.value = parseInt(braco.value) + 1
+});
+
+subtrair.addEventListener('click',(evento) => {
+    braco.value = parseInt(braco.value) - 1
+});
