ae7219b1c3e056f40ac935fc643ab0d6549d69ea
renata-aline <renataaline@gmail.com>
2023-05-22 18:06:43 -0300

    reaproveitando código

--- js/main.js
+++ js/main.js
@@ -11,10 +11,14 @@ const subtrair = document.querySelector('#subtrair');
 const somar = document.querySelector('#somar');
 const braco = document.querySelector('#braco');
 
-somar.addEventListener('click',(evento) => {
-    braco.value = parseInt(braco.value) + 1
-});
+somar.addEventListener('click', () => {manipulaDados('somar')});
 
-subtrair.addEventListener('click',(evento) => {
-    braco.value = parseInt(braco.value) - 1
-});
+subtrair.addEventListener('click',() => {manipulaDados("subtrair")});
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
